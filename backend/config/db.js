710756a7a9773cef752b3dfd78fa7134b66b289a
anas <anas98955559625@gmail.com>
2024-12-02 08:23:44 +0400

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,14 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const dbURI = process.env.MONGO_DB_URI;
+    const dbURI = process.env.MONGO_DB_URI; 
     await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("MongoDB connected successfully.".bgWhite.green);
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    console.error(`MongoDB connection error: ${error.message}`.bgRed.white);
     process.exit(1);
   }
 };
