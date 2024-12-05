889912be897d5ec836afbfd39e989a5656ad9472
anas <anas98955559625@gmail.com>
2024-12-05 16:13:40 +0400

    updated

--- client/src/utils/axiosInstance.jsx
+++ client/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8050", // Dynamically sets the backend URL
+  baseURL: "https://logos-mart-server.onrender.com", // Dynamically sets the backend URL
   withCredentials: true, // Ensures cookies are sent with requests
 });
 
